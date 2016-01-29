3ddcc427d45556c04b43df5c2cc7b71994ba8ee4
Chris Foster <chris.foster@roames.com.au>
2016-01-29 17:28:03 +1000

    PR-2356 Working version of PointCloud
    Strip out the worst of the PointMatching-specific functionality, and
    start refactoring into a generic point cloud data structure.  Move to
    the convention that point attribute columns are Vector{T} rather than
    general arrays with the last dimension indexing the points.  In line
    with this, start using FixedSizeArrays.Vec{3} for position and normal
    information.

--- src/PointClouds.jl
+++ src/PointClouds.jl
@@ -1,6 +1,7 @@
 module PointClouds
 
 using NearestNeighbors
+using FixedSizeArrays
 
 export PointCloud,
     # Point cloud data access
@@ -8,79 +9,109 @@ export PointCloud,
     # Data handling
     readdlm_points, split_cloud
 
+import Base:
+    show, length, getindex, setindex!, keys, haskey, vcat
+
+import NearestNeighbors:
+    knn
+
 
 """
-A PointCloud contains information about the 3D coordinates of the points in the
+A PointCloud contains information about the coordinates of the points in the
 cloud, an efficient data structure for spatial search, and various attributes
-which can be accessed by name.
-
-    points - 3xN array containing the 3D coordinates of the points composing
-             the cloud (TODO: Make this an array of fixed size arrays?)
+which can be accessed by name.  The only required attribute is `position`,
+which is used during spatial lookup.
 """
-type PointCloud{T}
-    points::Array{Float64, 2}
-    tree::T
-    attributes::Dict{Symbol,Array}
+type PointCloud{Dim,T,SIndex}
+    positions::Vector{Vec{Dim,T}}
+    spatial_index::SIndex
+    attributes::Dict{Symbol,Vector}
+end
+
+PointCloud(positions) = PointCloud(positions, KDTree(destructure(positions)),
+                                   Dict{Symbol,Vector}(:position=>positions))
+
+function show(io::IO, cloud::PointCloud)
+    print(io, "PointCloud(N=$(length(cloud)), attributes=$(keys(cloud.attributes)))")
 end
 
 
-PointCloud(points) = PointCloud(points, KDTree(points), Dict{Symbol,Array}())
+#------------------------
+# Container functionality acting on columns
+keys(cloud::PointCloud) = keys(cloud.attributes)
+haskey(cloud::PointCloud, attrname::Symbol) = (attrname == :position) || haskey(cloud.attributes, attrname)
 
-function Base.show(io::IO, pc::PointCloud)
-    print(io, "PointCloud(N=$(length(pc)), attributes=$(keys(pc.attributes)))")
+function getindex(cloud::PointCloud, attrname::Symbol)
+    haskey(cloud, attrname) || error("Point cloud $cloud has no attribute $attrname")
+    cloud.attributes[attrname]
 end
 
-# Attribute access
-Base.getindex(pc::PointCloud, i) = pc.points[:,i]
-
-function Base.getindex(pc::PointCloud, attrname::Symbol)
-    haskey(pc, attrname) || error("Point cloud $pc has no attribute $attrname")
-    pc.attributes[attrname]
-end
-
-function Base.setindex!(pc::PointCloud, value::Array, attrname::Symbol)
-    if size(value)[end] != length(pc)
-        error("Length of attribute $attrname not equal to number of points = $(length(pc))")
+function setindex!(cloud::PointCloud, value::Vector, attrname::Symbol)
+    if length(value) != length(cloud)
+        error("length($attrname) = $(length(value)) not equal to number of points = $(length(cloud))")
     end
-    pc.attributes[attrname] = value
+    cloud.attributes[attrname] = value
 end
 
-Base.haskey(pc::PointCloud, attrname::Symbol) = (attrname == :position) || haskey(pc.attributes, attrname)
+"Return vector of point cloud positions"
+positions(cloud::PointCloud)   = cloud.positions
 
-function Base.vcat{T}(cloud1::PointCloud{T}, clouds::PointCloud{T}...)
-    # FIXME dispatch not seeing this...
-    out = deepcopy(cloud1)
-    ks = Set(keys(clouds1))
+"Return array of point cloud normals as fixed size vectors"
+function normals{Dim,T,SIndex}(cloud::PointCloud{Dim,T,SIndex})
+    cloud.attributes[:normal]::Vector{Vec{Dim,Float32}}
+end
+
+
+#--------------------------
+# Container functionality acting on rows
+length(cloud::PointCloud) = length(cloud.positions)
+
+function vcat{Dim,T,SIndex}(cloud1::PointCloud{Dim,T,SIndex}, clouds::PointCloud{Dim,T,SIndex}...)
+    attrs = deepcopy(cloud1.attributes)
+    ks = Set(keys(attrs))
     for cloud in clouds
-        ki = Set(keys(clouds))
+        ki = Set(keys(cloud))
         ki == ks || error("Cannot join clouds with inconsistent attributes")
-        # FIXME: This is all an ugly hack!
-        out.points = hcat(out.points, cloud.points)
-        for (k,v) in out.attributes
-            if ndims(val) == 1
-                append!(v, cloud.attributes[k])
-            else
-                out.attributes[k] = hcat(out.attributes[k], cloud.attributes[k])
-            end
+        for (k,v) in attrs
+            append!(v, cloud.attributes[k])
         end
     end
+    pos = attrs[:position]
+    out = PointCloud{Dim,T,SIndex}(pos, KDTree(destructure(pos)), attrs)
     out
 end
 
-# Position attribute
+"""
+Group points into `M = unique(scanid)` point clouds, each containing points
+with the same scan id.  Returns a dictionary mapping the unique scanid to each
+cloud.
+"""
+function split_cloud(allpoints, scanid; min_points::Int=10000)
+    unique_scans = unique(scanid)
+    scans = Dict{eltype(unique_scans), typeof(allpoints)}()
+    for id in unique_scans
+        sel = id .== scanid
+        pos = positions(allpoints)[sel]
+        cloud = PointCloud(pos)
+        for (name,val) in allpoints.attributes
+            cloud[name] = val[sel]
+        end
+        scans[id] = cloud
+    end
+    scans
+end
 
-"Return 3xN array of point cloud positions"
-positions(pc::PointCloud)   = pc.points
-Base.size(pc::PointCloud) = size(pc.points) # FIXME, this may not make sense?
-Base.length(pc::PointCloud) = size(pc.points,2)
 
-# Fast spatial knn lookup
-NearestNeighbors.knn(pc::PointCloud, point, k) = knn(pc.tree, point, k)
+#--------------------------
+# Spatial indexing functionality
+knn(cloud::PointCloud, point, k) = knn(cloud.spatial_index, point, k)
 
 
+#------------------------
+# File IO
 # Column names and formats for Roames point cloud text format :(
 const text_column_info = Dict(
-    :X =>  (Float64, :position),  # TODO: Systematically aggregate position
+    :X =>  (Float64, :position),  # TODO: Systematically aggregate position into a vector.
     :Y =>  (Float64, :position),
     :Z =>  (Float64, :position),
     :G =>  (Float32, :ground_height),
@@ -93,10 +124,10 @@ const text_column_info = Dict(
     :P =>  (Int8,    :number_of_returns),
     :C =>  (UInt8,   :classification),
     :I =>  (UInt16,  :intensity),
-    :L =>  (UInt16,  :point_source),
-    :S =>  (Int64,   :flight_strip),
+    :L =>  (UInt16,  :point_source_id),
+    :S =>  (Int64,   :flight_strip_id),
     :U =>  (Int,     :cluster),
-    :Nx => (Float32, :normal_x), # TODO: Aggregate the normal info
+    :Nx => (Float32, :normal_x), # TODO: Aggregate any normal info into a vector
     :Ny => (Float32, :normal_y),
     :Nz => (Float32, :normal_z),
 )
@@ -114,43 +145,50 @@ for mapping details.)
 function readdlm_points(source::IO)
     data, column_names = readdlm(source, header=true)
 
+    npoints = size(data,1)
     xyz_names = ["X", "Y", "Z"]
     xyz_cols = Int[findfirst(column_names, n) for n in xyz_names]
-    # TODO: Use FixedSizeArrays for this?
-    points = PointCloud(CloudId(-1,-1), data[:, xyz_cols]')
+    positions = Vector{Vec{3,Float64}}(npoints)
+    @fslice positions[:,:] = data[:, xyz_cols]'
+    cloud = PointCloud(positions)
 
     for (cindex, cname) in enumerate(column_names)
+        if cname in xyz_names
+            continue
+        end
         cname = Symbol(cname)
-        cname ∉ xyz_names || continue
         ctype,fullname = text_column_info[cname]
-        points[fullname] = map(ctype, data[:,cindex])
+        cloud[fullname] = map(ctype, data[:,cindex])
     end
-    points
+    cloud
 end
 
 readdlm_points(filename::AbstractString) = open(io->readdlm_points(io), filename, "r")
 
 
+#------------------------
+# Utilities for adding columns
+
 """
-    pca_normals(points, tree; neighbours::Int=10)
+    pca_normals!(normals, query_points, cloud; neighbours::Int=10)
 
 Compute normals of a point cloud via PCA
 
-For each point in the 3xN array `points`, compute an approximate surface normal
-using PCA on the local neighbourhood, using the closest `neighbours` points.
-`tree` should be an spatial lookup datastructure supporting the function `knn()`.
-The normals are returned in a 3xN array.
+For each point in the 3xN array `query_points`, compute an approximate surface
+normal using PCA on the local neighbourhood, using the closest `neighbours`
+points.  `cloud` should support spatial lookup via the `knn()` function.
+Normals are returned in the 3xN array `normals`.
+
+TODO: Need a generalized version of this for the PCA covariance computation.
 """
-function pca_normals(points, tree; neighbours::Int=10)
-    # Use Float32 for estimated normals - heaps of precision at half the memory
-    normals = similar(points, Float32)
-    for i = 1:size(points,2)
-        p = points[:,i]
-        inds,_ = knn(tree, p, neighbours)
+function pca_normals!(normals, query_points, cloud; neighbours::Int=10)
+    for i = 1:size(query_points,2)
+        p = query_points[:,i]
+        inds,_ = knn(cloud, p, neighbours)
         if isempty(inds)
             continue
         end
-        d = (points[:,inds] .- p)
+        d = (query_points[:,inds] .- p)
         # Select evec of smallest eval
         evals, evecs = eig(Symmetric(d*d'), 1:1)
         #print("$evals")
@@ -160,43 +198,18 @@ function pca_normals(points, tree; neighbours::Int=10)
 end
 
 
-"Return 3xN array of point cloud normals.  Generated on demand.
-
-TODO: This is very specific to point matching at the moment.  For other uses,
-we will want other ways to index the local environment, so generating this
-magically on demand is really less than ideal.
-"
-function normals(cloud::PointCloud)
-    if !haskey(cloud.attributes, :normal)
-        cloud.attributes[:normal] = pca_normals(cloud.points, cloud.tree)
-    end
-    cloud.attributes[:normal]::Array{Float32,2}
-end
-
-
 """
-Group points into `M = unique(scanid)` point clouds, each containing points
-with the same scan id.  Returns a dictionary mapping the unique scanid to each
-cloud.
+Add normals to point cloud in Float32 precision, compute using PCA.
+
+See pca_normals!() for details.
 """
-function split_cloud(allpoints, scanid; min_points::Int=10000)
-    unique_scans = unique(scanid)
-    scans = Dict{eltype(unique_scans), typeof(allpoints)}()
-    for id in unique_scans
-        sel = id .== scanid
-        pos = positions(allpoints)[:,sel]
-        cloud = PointCloud(CloudId(id,0), pos)
-        for (name,val) in allpoints.attributes
-            # FIXME: This is an ugly hack!
-            if ndims(val) == 1
-                cloud[name] = val[sel]
-            else
-                cloud[name] = val[:,sel]
-            end
-        end
-        scans[id] = cloud
-    end
-    scans
+function add_normals!{Dim}(cloud::PointCloud{Dim}; kwargs...)
+    # Use Float32 for estimated normals - heaps of precision at half the memory
+    query_points = positions(cloud)
+    normals = similar(query_points, Vec{Dim,Float32})
+    pca_normals!(destructure(normals), destructure(query_points), cloud; kwargs...)
+    cloud[:normals] = normals
+    cloud
 end
 
 

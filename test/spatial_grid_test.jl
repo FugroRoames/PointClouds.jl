2aae86f025b25a4311cc2dc9a11cc0c7ad3366c9
Josh Christie <joshua.christie@roames.com.au>
2016-06-30 18:02:58 +1000

    Improve code coverage and small fixes to rasterizer

--- test/spatial_grid_test.jl
+++ test/spatial_grid_test.jl
@@ -1,51 +1,68 @@
 @testset "SparseVoxelGrid tests" begin
-    @testset "Voxelization" begin
-        # Create points for a uniformly spaced 4x4x4 grid
-        x = collect(linspace(0.0, 3.0, 4))
-        cnt = 1
-        points = zeros(3, 64)
-        for i in x, j in x, k in x
-            points[1, cnt] = i
-            points[2, cnt] = j
-            points[3, cnt] = k
-            cnt += 1
-        end
+    # Create points for a uniformly spaced 4x4x4 grid
+    x = collect(linspace(0.0, 3.0, 3))
+    cnt = 1
+    points = zeros(3, 27)
+    for i in x, j in x, k in x
+        points[1, cnt] = i
+        points[2, cnt] = j
+        points[3, cnt] = k
+        cnt += 1
+    end
+    cloud = PointCloud(points)
 
+    @testset "Rasterizer" begin
         # Test standard uniformly sized voxels
-        voxel_size = 1.51
-        grid = SparseVoxelGrid(points, voxel_size)
-        @test length(collect(grid)) == 8
-        [@test length(collect(voxel)) == 8 for voxel in grid]
+        d = rasterize_points(points, 1.0)
+        [@test length(r) == 3 for r in values(d)]
 
         # Test a point cloud
-        cloud = PointCloud(points)
+        d = rasterize_points(cloud, 1.0)
+        [@test length(r) == 3 for r in values(d)]
+    end
+
+    @testset "Voxelization" begin
+        # Test standard uniformly sized voxels
+        voxel_size = 1.0
+        grid = SparseVoxelGrid(points, voxel_size)
+        @test isempty(grid) == false
+        @test haskey(grid, (0,0,0)) == true
+        @test length(grid) == 27
+        @test length(collect(grid)) == 27
+        [@test length(collect(voxel)) == 1 for voxel in grid]
+
         grid = SparseVoxelGrid(cloud, voxel_size)
-        [@test length(collect(voxel)) == 8 for voxel in grid]
+        [@test length(collect(voxel)) == 1 for voxel in grid]
+        @test voxel_center(grid, (1, 1, 1)) == Vec(0.5, 0.5, 0.5)
 
         # Test voxels with different side lengths in each axis
-        grid = SparseVoxelGrid(cloud, (voxel_size, voxel_size, 4.0))
-        [@test length(collect(voxel)) == 16 for voxel in grid]
-        grid = SparseVoxelGrid(points, (voxel_size, 4.0, 4.0))
-        [@test length(collect(voxel)) == 32 for voxel in grid]
+        @test length(collect(SparseVoxelGrid(cloud, (2.0, 2.5, 4.0)))) == 4
     end
 
     @testset "Neighbouring voxel" begin
-        srand(1)
-        points = rand(3, 10000) * 10.0
-        grid = SparseVoxelGrid(points, 5.0)
         radius = 1
+        grid = SparseVoxelGrid(points, 2.0)
         for voxel in grid
-            @test length(collect(in_cuboid(grid, voxel, radius))) == 7
+            # Test using anonymous function method
+            in_cuboid(grid, voxel, radius) do voxel
+                @test haskey(grid, voxel.id)
+            end
         end
 
-        grid = SparseVoxelGrid(points, 2.5)
-        voxel_list = [(1,1,1), (1,2,1), (2,2,2)]
-        @test length(collect(in_cuboid(grid, voxel_list[1], radius))) == 26
-        @test length(collect(in_cuboid(grid, voxel_list[2], radius))) == 26
-        @test length(collect(in_cuboid(grid, voxel_list[3], 2))) == length(grid)-1
+        grid = SparseVoxelGrid(points, 1.5)
+        voxel_list = [(1,1,1), (1,2,1), (2,2,2), (10,10,10)]
+        @test length(collect(in_cuboid(grid, voxel_list[1], 2))) == 26
+        @test length(collect(in_cuboid(grid, voxel_list[2], 2))) == 26
+        @test length(collect(in_cuboid(grid, voxel_list[3], 2))) == 26
+        @test length(collect(in_cuboid(grid, voxel_list[4], 2))) == 0
+        @test length(collect(in_cuboid(grid, collect(grid)[1], 2))) == 26
 
-        in_cuboid(grid, (1,1,1), radius) do voxel
-            @test haskey(grid, voxel.id)
-        end
+        # Test show methods
+        io = IOBuffer()
+        voxel = collect(grid)[1]
+        cuboid = in_cuboid(grid, (0,0,0), radius)
+        show(io, grid)
+        show(io, voxel)
+        show(io, cuboid)
     end
 end
